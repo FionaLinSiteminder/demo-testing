b7e130c7a1f2b6681bffb4e0c8add6444f93801e
Fi <85475527+FionaLinSiteminder@users.noreply.github.com>
2021-10-01 11:20:30 +1000

    test

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -9,10 +9,14 @@ const routes: RouteRecordRaw[] = [
 
   // Always leave this as last one,
   // but you can also remove it
-  {
+               {
     path: '/:catchAll(.*)*',
-    component: () => import('pages/Error404.vue'),
-  },
+    component: () =>
+      import('pages/Error404.vue'),
+  }
+
+
+
 ];
 
 export default routes;
